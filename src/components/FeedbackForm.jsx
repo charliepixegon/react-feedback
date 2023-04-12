08c22fed8899ba70cf5d48643b5795d02359ec7c
Charlie Fulton <charlie@pixegon.com>
2023-04-12 12:36:41 -0400

    start of form

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import Card from './shared/Card';
+
+function FeedbackForm() {
+  const [reviewText, setReviewText] = useState('');
+
+  const handleReviewTextChange = (e) => {
+    console.log(e.target.value);
+    setReviewText(e.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* @todo rating select component */}
+        <div className="input-group">
+          <input
+            onChange={handleReviewTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={reviewText}
+          />
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
