256a2c91e2b0c1940ad1a52766fd31f9ef2675a9
Charlie Fulton <charlie@pixegon.com>
2023-04-16 16:38:36 -0400

    fixed warning

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Route is a renderer of a component
 import Header from './components/Header';
 
-import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
